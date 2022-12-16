06c67992e98a158f1fccbdc36072dc20144dc03a
p3lican <365mardigras@gmail.com>
2022-12-16 17:28:08 -0600

    asd

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,6 +56,6 @@ export default {
     baseUrl:
       process.env.NODE_ENV !== 'production'
         ? 'http://localhost:8888'
-        : 'https://shopify-nuxt-kit.netlify.app',
+        : 'https://heroic-strudel-7b1f72.netlify.app',
   },
 }
